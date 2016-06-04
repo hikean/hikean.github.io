8c1cc2bfe19432add4fd2391ee3fe76a56792661
iKeansMac <imkean@163.com>
2016-06-04 13:41:39 +0800

    add home page

---
+++ js/particle_setting.js
@@ -0,0 +1,33 @@
+/**
+ * Particleground demo
+ * @author Jonathan Nicol - @mrjnicol
+ */
+
+document.addEventListener('DOMContentLoaded', function () {
+  particleground(document.getElementById('particles'), {
+    dotColor: '#eee'/*'#5cbdaa'*/,
+    lineColor: '#ddd'/*'#5cbdaa'*/,
+    particleRadius: 15,
+    density: 20000,
+    parallaxMultiplier: 20,
+    proximity: 256,
+    parallax: true,
+    lineWidth: 1,
+  });
+  var intro = document.getElementById('intro');
+  intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
+}, false);
+
+
+/*
+// jQuery plugin example:
+$(document).ready(function() {
+  $('#particles').particleground({
+    dotColor: '#5cbdaa',
+    lineColor: '#5cbdaa'
+  });
+  $('.intro').css({
+    'margin-top': -($('.intro').height() / 2)
+  });
+});
+*/
