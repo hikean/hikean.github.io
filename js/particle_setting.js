d8cb61960982ca011e436370e5ea4c66b27d8107
iKeansMac <imkean@163.com>
2016-06-04 22:51:29 +0800

    ..

--- js/particle_setting.js
+++ js/particle_setting.js
@@ -5,14 +5,14 @@
 
 document.addEventListener('DOMContentLoaded', function () {
   particleground(document.getElementById('particles'), {
-    dotColor: '#eee'/*'#5cbdaa'*/,
-    lineColor: '#ddd'/*'#5cbdaa'*/,
-    particleRadius: 15,
-    density: 20000,
+    dotColor: '#fff'/*'#5cbdaa'*/,
+    lineColor: '#fff'/*'#5cbdaa'*/,
+    particleRadius: 4,
+    density: 8000,
     parallaxMultiplier: 20,
-    proximity: 256,
+    proximity: 72,
     parallax: true,
-    lineWidth: 1,
+    lineWidth: 0.6,
   });
   var intro = document.getElementById('intro');
   intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
